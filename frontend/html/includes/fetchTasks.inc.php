f8ba7d5714af0098bf34e16fa7e4b48ab965dbd8
Inkyubyte <phantom1162@gmail.com>
2024-12-06 13:20:57 -0700

    Completed Filtering

--- frontend/html/includes/fetchTasks.inc.php
+++ frontend/html/includes/fetchTasks.inc.php
@@ -12,16 +12,50 @@ if (!isset($_SESSION["userid"])) {
 # Grab logged in user
 $userID = $_SESSION["userid"];
 
+$data = json_decode(file_get_contents("php://input"), true);
+$statuses = isset($data['statuses']) ? $data['statuses'] : [];
+$categories = isset($data['categories']) ? $data['categories'] : [];
+
+$sql = "SELECT task.taskid, task.title, task.duedate, task.status, category.name AS categoryname FROM task WHERE userid = $1";
+
 $sql = 'SELECT task.taskid, task.title, task.duedate, task.status, category.name AS categoryname
 FROM task
 INNER JOIN category
 ON task.categoryid = category.categoryid
-WHERE task.userid = $1';
-$result = pg_query_params($conn, $sql, array($userID));
+WHERE task.userid = $1 ';
+
+$params = [$_SESSION['userid']];
+$paramsCount = 2;
+
+if(!empty($statuses)) {
+    $statusFilters = [];
+
+    foreach ($statuses as $status) {
+        $statusFilters[] = '$' . $paramsCount;
+        $params[] = $status;
+        $paramsCount++;
+    }
+
+    $sql .= "AND task.status IN (" . implode(", ", $statusFilters) . ")";
+}
+
+
+if(!empty($categories)) {
+    $categoryFilters = [];
+
+    foreach ($categories as $category) {
+        $categoryFilters[] = '$' . $paramsCount;
+        $params[] = $category;
+        $paramsCount++;
+    }
+
+    $sql .= "AND task.categoryid IN (" . implode(", ", $categoryFilters) . ")";
+}
+
+$result = pg_query_params($conn, $sql, $params);
 
 if (!$result) {
-    $tasks = [];
-    echo json_encode($tasks);
+    echo json_encode(["error" => "Error: Unable to fetch tasks"]);
     exit();
 }
 
